060e2d21cc2bb8478589055ae9b0016eca5d2338
woseayrch <3303341078@qq.com>
2026-01-21 23:14:48 +0800

    Update 32_node3.sh

--- scripts/32_node3.sh
+++ scripts/32_node3.sh
@@ -74,7 +74,7 @@ args="
 torchrun --nnodes=4 \
          --nproc_per_node=8 \
          --node_rank=3 \
-         --master_addr=100.65.39.222 \
+         --master_addr=100.65.60.61 \
          --master_port=30000 \
          --max_restarts=10 \
          ${entry_file} ${args}
