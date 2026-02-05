310909fa7633ce80960e7e60d77eff9f6efda403
woseayrch <3303341078@qq.com>
2026-02-05 17:07:30 +0800

    Update train_amlt.sh

--- scripts/train_amlt.sh
+++ scripts/train_amlt.sh
@@ -25,8 +25,8 @@ datasets=0_30_s_academic_v0_1,0_30_s_youtube_v0_1,0_30_s_activitynetqa,0_30_s_ne
 # datasets=0_30_s_academic_v0_1,0_30_s_youtube_v0_1,0_30_s_activitynetqa,0_30_s_nextqa,0_30_s_perceptiontest
 
 # Output configuration
-run_name="qwen3vl_2b_single_ema_codebook"
-output_dir=/blob/dyb_output/icml2026/qwen3vl_2b_single_ema_codebook_5e_6
+run_name="qwen3vl_2b_orthus"
+output_dir=/blob/dyb_output/icml2026/qwen3vl_2b_orthus
 export WANDB_PROJECT="icml_ar_ablation"
 
 # Training arguments
