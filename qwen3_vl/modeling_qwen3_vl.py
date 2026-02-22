98e2ad80a74f7f0b0309c9b90c9b86512088a855
woseayrch <3303341078@qq.com>
2026-02-22 13:21:03 +0800

    Update modeling_qwen3_vl.py

--- qwen3_vl/modeling_qwen3_vl.py
+++ qwen3_vl/modeling_qwen3_vl.py
@@ -1415,7 +1415,10 @@ class Qwen3VLForConditionalGeneration(Qwen3VLPreTrainedModel, GenerationMixin):
     def __init__(self, config):
         super().__init__(config)
         self.model = Qwen3VLModel(config)
-        self.vision_vocab_size = self.model.visual.vq.quantize.codebook.shape[0] + 1
+        vision_vocab_size = self.model.visual.vq.quantize.codebook.shape[0]
+        self.vision_vocab_size = vision_vocab_size + 1
+        self.vision_embedding = nn.Embedding(vision_vocab_size, hidden_size)
+        self.vision_embedding.weight.data[:vision_vocab_size] = self.model.visual.vq.quantize.codebook.clone()
         self.lm_head = nn.Linear(config.text_config.hidden_size, config.text_config.vocab_size, bias=False)
         self.gen_head_0 = nn.Linear(config.text_config.hidden_size, self.vision_vocab_size, bias=False)
         self.gen_head_1 = nn.Linear(config.text_config.hidden_size, self.vision_vocab_size, bias=False)
