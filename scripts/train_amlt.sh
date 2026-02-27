ca6727a29371fe4b5da234d40a6614ca0540d92c
woseayrch <3303341078@qq.com>
2026-02-27 23:33:55 +0800

    Update train_amlt.sh

--- scripts/train_amlt.sh
+++ scripts/train_amlt.sh
@@ -11,6 +11,7 @@ deepspeed=./scripts/zero2.json
 # Model configuration
 llm=Qwen/Qwen3-VL-2B-Instruct  # Using HuggingFace model ID
 llm=/blob/dyb_output/icml2026/qwen3vl_2b_single_ema_codebook_uni_image_pretrain/checkpoint-77891
+llm=/blob/dyb_output/icml2026/qwen3vl_2b_single_ema_codebook_uni_video_pretrain/checkpoint-42910
 vq_path=/blob/dyb_output/icml2026/multiple_codebook_ema_scale_image_video/checkpoint-963425/model.safetensors
 
 # Training hyperparameters
@@ -26,8 +27,8 @@ datasets=0_30_s_academic_v0_1,0_30_s_youtube_v0_1,0_30_s_activitynetqa,0_30_s_ne
 # datasets=0_30_s_academic_v0_1,0_30_s_youtube_v0_1,0_30_s_activitynetqa,0_30_s_nextqa,0_30_s_perceptiontest
 
 # Output configuration
-run_name="qwen3vl_2b_single_ema_codebook_uni_video_pretrain"
-output_dir=/blob/dyb_output/icml2026/qwen3vl_2b_single_ema_codebook_uni_video_pretrain
+run_name="qwen3vl_2b_single_ema_codebook_uni_video_sft"
+output_dir=/blob/dyb_output/icml2026/qwen3vl_2b_single_ema_codebook_uni_video_sft
 export WANDB_PROJECT="icml_ar_ablation"
 
 # Training arguments
@@ -60,9 +61,9 @@ args="
     --save_steps 5000 \
     --save_total_limit 100 \
     --learning_rate ${lr} \
-    --weight_decay 0.01 \
+    --weight_decay 0.00 \
     --max_grad_norm 1 \
-    --lr_scheduler_type "constant" \
+    --lr_scheduler_type "cosine_with_min_lr" \
     --logging_steps 20 \
     --model_max_length 32768 \
     --gradient_checkpointing True \
